ef8535d45ab4d971ef9155d46a3ed114d443437c
wswoodruff <williamswoodruff@gmail.com>
2022-03-17 23:53:41 -0400

    Add models, render Curiosity

--- src/Scene.js
+++ src/Scene.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber'
+import { Loader } from '@react-three/drei';
 
 import Box from './components/Box';
 import Cam from './components/Cam';
@@ -7,14 +8,12 @@ import Text from './components/Text';
 import Bg from './components/Bg';
 import Info from './components/Info';
 
+import Curiosity from './models/Curiosity';
+
 const GH_REPO = 'https://github.com/wswoodruff/three-fiber-lab';
 // const PROGRAMMER_RED = 'rgb(255, 0, 0)';
 const PROGRAMMER_GREEN = 'rgb(0, 255, 0)';
 const PROGRAMMER_BLUE = 'rgb(0, 0, 255)';
-
-const initCamConfig = {
-    position: [2, 10, 10]
-};
 
 const gridHelperConfig = {
     // args: [ size, divisions, centerLineColor, gridColor ]
@@ -23,21 +22,36 @@ const gridHelperConfig = {
 
 export default function Scene () {
 
+    // const Curiosity = useGLTF('models/glb/curiosity.glb');
+
+    // console.log("Curiosity", Curiosity);
+    // const curiosity = ModelManager('/models/glb/curiosity.glb', {
+    //     //
+    // });
+
     return (
-      <Canvas camera={initCamConfig}>
-        <directionalLight color="#ffffff" intensity={0.7} />
-        <ambientLight color="#ffffff" intensity={0.2} />
-        <Suspense fallback={null}>
-            <Bg />
-            <Cam />
-            <Box position={[0, 3, 0]} />
-            <Text onClick={() => window.open(GH_REPO)}>
-              Yooooo!
-            </Text>
-            {/* <Text test /> */}
-            <gridHelper {...gridHelperConfig} />
-            <Info />
-        </Suspense>
-      </Canvas>
+        <>
+            <Canvas>
+                <Suspense fallback={null}>
+                    <directionalLight color="#ffffff" intensity={0.7} />
+                    <ambientLight color="#ffffff" intensity={0.2} />
+                    <Bg />
+                    <Cam />
+                    {/* <Box position={[0, 3, 0]} /> */}
+                    <Text
+                        onClick={() => window.open(GH_REPO)}
+                        position={[0, 1, -10]}
+                    >
+                        Zill's Lab
+                    </Text>
+                    <Curiosity scale={2.4} />
+                    <gridHelper {...gridHelperConfig} />
+                    <Info />
+                </Suspense>
+            </Canvas>
+            {/* Supposedly loads the scene w/ a progress bar but
+              * I haven't seen it yet cuz the scene is light right now */}
+            <Loader />
+        </>
     );
 };
--- src/components/Cam.js
+++ src/components/Cam.js
@@ -3,42 +3,34 @@ import {
     // CubeCamera,
     OrbitControls,
     GizmoHelper as AxisGizmoHelper,
-    GizmoViewport as AxisGizmoViewport,
-    GizmoViewcube
+    GizmoViewport as AxisGizmoViewport
 } from '@react-three/drei';
-
-const orbitControlsConfig = {
-    autoRotate: true,
-    autoRotateSpeed: 1
-}
-
-const gizmoViewportConfig = {
-    axisColors: ['red', 'green', 'blue'],
-    labelColor: 'black'
-};
 
 export default function Cam(props) {
 
     const {
-        autoRotate = orbitControlsConfig.autoRotate,
-        autoRotateSpeed = orbitControlsConfig.autoRotateSpeed
+        orbit = true,
+        autoRotate = true,
+        autoRotateSpeed = 1
     } = props;
 
     return (
         <>
-            {/* <CubeCamera makeDefault /> */}
-            {/* <PerspectiveCamera args={[2, 10, 4]} /> */}
-            <OrbitControls
+            <PerspectiveCamera makeDefault position={[4, 11, 15]} />
+            {orbit && <OrbitControls
                 autoRotate={autoRotate}
                 autoRotateSpeed={autoRotateSpeed}
-            />
+            />}
             <AxisGizmoHelper
                 alignment="bottom-right" // widget alignment within scene
                 margin={[80, 80]} // widget margins (X, Y)
-                // onUpdate={ called during camera animation  }
+                // onUpdate={ called during camera animation }
                 // onTarget={ return current camera target (e.g. from orbit controls) to center animation }
             >
-                <AxisGizmoViewport {...gizmoViewportConfig} />
+                <AxisGizmoViewport
+                    axisColors={['red', 'green', 'blue']}
+                    labelColor={'black'}
+                />
             </AxisGizmoHelper>
         </>
     );
